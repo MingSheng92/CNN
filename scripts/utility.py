0710c009f91e68839a2b4d250f66524dbc570c9c
MingSheng92 <58675240+MingSheng92@users.noreply.github.com>
2019-12-21 15:57:45 +1100

    Add files via upload

--- scripts/utility.py
+++ scripts/utility.py
@@ -8,6 +8,51 @@ DG_LABEL = ['0','1', '2', '3', '4', '5', '6', '7', '8', '9']
 
 #cpu_model = NN_test.CNN_model.sync_to_cpu()
 
+def plot_predictions(dataset, predictions, true_labels, correct=True):
+    # set label data for plot later
+    if dataset == 'mnist':
+        LABEL_NAMES = DG_LABEL
+    elif dataset == 'fashion_mnist':
+        LABEL_NAMES = FM_LABEL
+    
+    label_string = "correct"
+    incorrect = np.where(predictions==true_labels)[0]
+    if correct:
+        label_string = "incorrect"
+        incorrect = np.where(predictions!=true_labels)[0]
+
+    print("Found {} {} labels" .format(len(incorrect), label_string))
+    
+    n = images.shape[0]
+    nc = int(np.ceil(n / 4))
+    fig = pyplot.figure(figsize=(4,3))
+    # axes = fig.add_subplot(nc, 4)
+    f, axes = pyplot.subplots(nc, 4)
+    f.tight_layout()
+  
+    for i in range(nc * 4):
+        y = i // 4
+        x = i % 4
+
+        axes[x, y].axis('off')
+    
+        label = LABEL_NAMES[np.argmax(predictions[incorrect[i]])]
+        confidence = np.max(predictions[incorrect[i]])
+        
+        if i > n:
+            continue
+        
+        axes[x, y].imshow(images[i])
+        pred_label = np.argmax(predictions[incorrect[i]])
+        axes[x, y].set_title("{} ({})\n {:.3f}".format(
+            LABEL_NAMES[pred_label], 
+            LABEL_NAMES[true_labels[incorrect[i]]],
+            confidence
+        ), color=("green" if true_labels[incorrect[i]] == pred_label else "red"))
+
+    pyplot.gcf().set_size_inches(8, 8)
+
+'''
 def plot_predictions(images, predictions, true_labels, dataset):
     # set label data for plot later
     if dataset == 'mnist':
@@ -43,3 +88,4 @@ def plot_predictions(images, predictions, true_labels, dataset):
         ), color=("green" if true_labels[i] == pred_label else "red"))
 
     pyplot.gcf().set_size_inches(8, 8)
+'''
